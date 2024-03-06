111f5af1fc38e20f10dfb3b1e245cc52c70bc7a4
JoaoLace <joaopaulolacerdaleao@gmail.com>
2024-03-06 20:40:10 -0300

    ><

--- js/sign.js
+++ js/sign.js
@@ -1,46 +1,56 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { getFirestore, doc, setDoc, add } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
 // Inicialize o aplicativo Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyC2wzob4G4hx0WZb4OgdXC_jsIG9gDrNwM",
-    authDomain: "ufsj-894af.firebaseapp.com",
-    projectId: "ufsj-894af",
-    storageBucket: "ufsj-894af.appspot.com",
-    messagingSenderId: "74536358217",
-    appId: "1:74536358217:web:f4868d9a2f016147674a98"
-  };
+    apiKey: "sua-api-key",
+    authDomain: "seu-auth-domain",
+    projectId: "seu-project-id",
+    storageBucket: "seu-storage-bucket",
+    messagingSenderId: "seu-messaging-sender-id",
+    appId: "sua-app-id"
+};
+
 const firebaseApp = initializeApp(firebaseConfig);
 
 // Obtenha a instância de autenticação
 const auth = getAuth(firebaseApp);
 
-if (document.getElementById("btn-login")){
-    alert("dsaugdiusahg")
-document.getElementById("btn-login").addEventListener("click", function () { 
-alert("udagsdiusga")
-    var email = document.getElementById("email-sign").value;
-    var senha1 = document.getElementById("senha1").value;
-    var senha2 = document.getElementById("senha2").value;
+if (document.getElementById("btn-login")) {
+    document.getElementById("btn-login").addEventListener("click", function () {
+        var email = document.getElementById("email-sign").value;
+        var senha1 = document.getElementById("senha1").value;
+        var senha2 = document.getElementById("senha2").value;
 
-    if (senha1 !== senha2) {
-        alert("As senhas não coincidem");
-        setTimeout(function () {
-            location.reload(true);
-        }, 100);
-    } else {
-        createUserWithEmailAndPassword(auth, email, senha1)
-            .then(() => {
-                // esconderCarregamento(); // Certifique-se de definir ou remover esta função
-        location.reload()
-                
-                window.location.href = "index.html";
-            })
-            .catch((error) => {
-                // esconderCarregamento(); // Certifique-se de definir ou remover esta função
-                alert(error);
-            });
-    }
-});
+        if (senha1 !== senha2) {
+            alert("As senhas não coincidem");
+            setTimeout(function () {
+                location.reload(true);
+            }, 100);
+        } else {
+            createUserWithEmailAndPassword(auth, email, senha1)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    const nomeUsuario = document.getElementById("nome-sign").value;
+
+                    const db = getFirestore();
+                    const userRef = doc(db, "users", user.uid);
+                    const dinheiro = 10000
+                    const userData = {
+                        nome: nomeUsuario,
+                        dinheiro: 10000.0
+                    };
+                    // Define os dados do usuário no Firestore
+                     return setDoc((userRef), userData);
+                   
+                })
+                .then(() => {
+                    window.location.href = "index.html";
+                })
+                .catch((error) => {
+                    console.error("Erro ao registrar o usuário:", error);
+                });
+        }
+    });
 }
