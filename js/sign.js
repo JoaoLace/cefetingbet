c696d144c4350fdfdddf84a6e9dc7cc5515eaf62
JoaoLace <joaopaulolacerdaleao@gmail.com>
2024-03-07 20:04:05 -0300

    ><

--- js/sign.js
+++ js/sign.js
@@ -1,56 +1,88 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, add } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+import { getFirestore, doc, setDoc, getDoc} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
-// Inicialize o aplicativo Firebase
 const firebaseConfig = {
-    apiKey: "sua-api-key",
-    authDomain: "seu-auth-domain",
-    projectId: "seu-project-id",
-    storageBucket: "seu-storage-bucket",
-    messagingSenderId: "seu-messaging-sender-id",
-    appId: "sua-app-id"
-};
+    apiKey: "AIzaSyC2wzob4G4hx0WZb4OgdXC_jsIG9gDrNwM",
+    authDomain: "ufsj-894af.firebaseapp.com",
+    databaseURL: "https://ufsj-894af-default-rtdb.firebaseio.com",
+    projectId: "ufsj-894af",
+    storageBucket: "ufsj-894af.appspot.com",
+    messagingSenderId: "74536358217",
+    appId: "1:74536358217:web:f4868d9a2f016147674a98"
+  };
+  
+const app = initializeApp(firebaseConfig);
 
-const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+document.addEventListener("DOMContentLoaded", function() {
+    // Verifique se o botão de login existe
+    if (document.getElementById("btn-login")) {
+        // Adicione um ouvinte de evento ao clique do botão de login
+        document.getElementById("btn-login").addEventListener("click", function () {
+            var email = document.getElementById("email-sign").value;
+            var senha1 = document.getElementById("senha1").value;
+            var senha2 = document.getElementById("senha2").value;
 
-// Obtenha a instância de autenticação
-const auth = getAuth(firebaseApp);
+            if (senha1 !== senha2) {
+                alert("As senhas não coincidem");
+                setTimeout(function () {
+                    location.reload(true);
+                }, 100);
+            } else {
+                createUserWithEmailAndPassword(auth, email, senha1)
+                    .then((userCredential) => {
+                        const user = userCredential.user;
+                        const nomeUsuario = document.getElementById("nome-sign").value;
 
-if (document.getElementById("btn-login")) {
-    document.getElementById("btn-login").addEventListener("click", function () {
-        var email = document.getElementById("email-sign").value;
-        var senha1 = document.getElementById("senha1").value;
-        var senha2 = document.getElementById("senha2").value;
+                        if (user) {
+                            const db = getFirestore(app);
+                            const userRef = doc(db, "users", user.uid);
+                            const userData = {
+                                nome: nomeUsuario,
+                            };
 
-        if (senha1 !== senha2) {
-            alert("As senhas não coincidem");
-            setTimeout(function () {
-                location.reload(true);
-            }, 100);
-        } else {
-            createUserWithEmailAndPassword(auth, email, senha1)
-                .then((userCredential) => {
-                    const user = userCredential.user;
-                    const nomeUsuario = document.getElementById("nome-sign").value;
+                            return setDoc(userRef, userData);
+                        } else {
+                            console.error("Usuário não autenticado.");
+                            throw new Error("Usuário não autenticado.");
+                        }
+                    })
+                    .then(() => {
+                        const user = auth.currentUser;
+                        const db = getFirestore(app);
+                        const userRef = doc(db, "users", user.uid);
 
-                    const db = getFirestore();
-                    const userRef = doc(db, "users", user.uid);
-                    const dinheiro = 10000
-                    const userData = {
-                        nome: nomeUsuario,
-                        dinheiro: 10000.0
-                    };
-                    // Define os dados do usuário no Firestore
-                     return setDoc((userRef), userData);
-                   
-                })
-                .then(() => {
-                    window.location.href = "index.html";
-                })
-                .catch((error) => {
-                    console.error("Erro ao registrar o usuário:", error);
-                });
-        }
-    });
+                        return getDoc(userRef)
+                            .then((doc) => {
+                                alert("dgsau");
+
+                                if (doc.exists()) {
+                                    const userData = doc.data();
+                                    const nomeDoUsuario = userData.nome;
+                                    const userInfoElement = document.getElementById("user-info");
+                                 
+                                }
+                            });
+                    })
+                    .then(() => {
+                        window.location.href = "index.html";
+                    })
+                    .catch((error) => {
+                        console.error("Erro ao registrar o usuário:", error);
+                    });
+            }
+        });
+    } else {
+        console.error("O botão de login não foi encontrado.");
+    }
+});
+
+if(window.location === "index.html")
+{
+    alert("rteste")
+     const userData = doc.data();
+     const nomeDoUsuario = userData.nome;
+     const userInfoElement = document.getElementById("user-info");
+     userInfoElement.textContent = "Bem-vindo, " + nomeDoUsuario;
 }
